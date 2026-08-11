5b88ff5347b8c49e89c9c627d685c5810c804414
Luckly809 <146329132+Luckly809@users.noreply.github.com>
2026-08-11 13:19:20 +1200

    11/8 SCORE
    got score working

--- node_2d.gd
+++ node_2d.gd
@@ -3,6 +3,8 @@ extends Node2D
 @export var mob_scene : PackedScene
 var timer = 1.0
 var toughness = 1.0
+var score = 0
+var score2 = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,8 +13,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
-
+	if score != score2:
+		print(score)
+		score2 = score
+	$Label.text = "Score : " + str(score)
 
 func _on_timer_timeout() -> void:
 	var mob = mob_scene.instantiate()
@@ -24,5 +28,4 @@ func _on_timer_timeout() -> void:
 		$Timer.wait_time = timer
 		timer = timer * 0.999
 		toughness *= 1.01
-		print(toughness)
 	add_child(mob)
